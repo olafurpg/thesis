5dd8e92fbd164793ae0295b24b075606375246ab
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-02-09 15:11:54 +0100

    New example.

---
+++ scalasphere/code/how-works.scala
@@ -0,0 +1,16 @@
+1. function(aaaaa, bbbbb.ccccc(ddddd))
+2. function(
+       aaaaa, bbbbb.ccccc(ddddd))
+3. function(aaaaa,
+            bbbbb.ccccc(ddddd))
+4. function(
+       aaaaa,
+       bbbbb.ccccc(ddddd))
+5. function(
+       aaaaa,
+       bbbbb
+         .ccccc(ddddd))
+6. function(
+       aaaaa,
+       bbbbb.ccccc(
+           ddddd))
