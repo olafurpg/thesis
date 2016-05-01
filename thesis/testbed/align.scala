f4ea2c6d6dd4bcff8c39772b79d7e0013758cbfb
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-05-01 07:06:43 +0200

    WIP

--- thesis/testbed/align.scala
+++ thesis/testbed/align.scala
@@ -1,5 +1,4 @@
  object host extends StringColumn(this) with PartitionKey[String]
   object date_time extends DateTimeColumn(this) with PartitionKey[DateTime]
-  object level extends StringColumn(this)
   object client_id extends OptionalStringColumn(this) with PartitionKey[Option[String]]
   //
