55c8c37d22b0188270e5f5be7cf4e10bf8b04d3f
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-05-14 11:43:51 +0200

    Scala Italy

---
+++ thesis/testbed/indent.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+
+func main() int {
+    DB.insert(User(1, "Marco", Friends(1,
+    2, 3), "secret-password",
+      3))
+}
--- thesis/testbed/indent.scala
+++ thesis/testbed/indent.scala
@@ -1,7 +1,8 @@
 object UserRepo {
   def update(): Unit = {
-    User(1, "Marco", Friends(
-      1, 2, 3), "secret-password",
-      3)
+    DB.insert(User(1, "Marco", Friends(
+      1, 2, 3
+    ), "secret-password",
+      3))
   }
 }
