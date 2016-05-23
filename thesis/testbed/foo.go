3fb0cf7ed6a85e4b787d440bc3318f29617b270d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-05-23 13:28:11 +0200

    WIP

--- thesis/testbed/foo.go
+++ thesis/testbed/foo.go
@@ -3,11 +3,12 @@ import "fmt"
 
 func main() int {
 
-Defn.Object(Nil,
-Term.Name("State"), Foo.Bar(1, Template(Nil, Seq( Ctor.Ref.Name("ScalaFmtLogger")),
-Term.Param(
-Nil,
-Name.Anonymous(), None, None)),
-Some()), Term.Name("gofmt") )
+Defn.Object(Nil, Term.Name("State"),
+    Foo.Bar(1, Template(Nil, Seq( Ctor.Ref.Name("ScalaFmtLogger")),
+    Term.Param(
+    Nil,
+    Name.Anonymous(), None, None)),
+    Some()), Term.Name("gofmt") )
+	res, err := format(fileSet, file, sourceAdj, indentAdj, src, printer.Config{Mode: printerMode, Tabwidth: tabWidth})
 
 }
--- thesis/testbed/foo.scala
+++ thesis/testbed/foo.scala
@@ -1,10 +1,12 @@
-object A {
-  Defn.Object(Nil,
+// column 80                                                                   |
+object Foo {
+Defn.Object(Nil,
 Term.Name("State"), Foo.Bar(1, Template(Nil, Seq( Ctor.Ref.Name("ScalaFmtLogger")),
 Term.Param(
 Nil,
 Name.Anonymous(), None, None)),
 Some()), Term.Name("gofmt") )
-javaBooleanAnd( javaBooleanAnd( javaBooleanAnd( javaBooleanAnd(javaBooleanAnd(javaBooleanAnd( javaBooleanAnd(javaBooleanAnd( javaBooleanAnd(javaBooleanAnd(javaBooleanAnd( _isEqualNodes(node.documentationComment, toNode.documentationComment), _isEqualNodeLists( node.metadata, toNode.metadata)), _isEqualTokens(node.abstractKeyword, toNode.abstractKeyword)), _isEqualTokens( node.classKeyword, toNode.classKeyword)), _isEqualNodes( node.name, toNode.name)), _isEqualNodes( node.typeParameters, toNode.typeParameters)), _isEqualNodes( node.extendsClause, toNode.extendsClause)), _isEqualNodes( node.withClause, toNode.withClause)), _isEqualNodes( node.implementsClause, toNode.implementsClause)), _isEqualTokens(node.leftBracket, toNode.leftBracket)), _isEqualNodeLists( node.members, toNode.members)), _isEqualTokens( node.rightBracket,
-            toNode.rightBracket))
+err := format(fileSet, file, sourceAdj, indentAdj, src, printer.Config(Mode= printerMode, Tabwidth= tabWidth))
+
+
 }
