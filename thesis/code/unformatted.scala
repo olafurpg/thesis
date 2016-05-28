aa000a2963ce6f048bea7fbe00b5398de215bc14
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-05-28 13:52:26 +0200

    WIP

--- thesis/code/unformatted.scala
+++ thesis/code/unformatted.scala
@@ -1,1 +1,11 @@
-object A {function(arg1, arg2(arg3(arg4, arg5, "arg6"), arg7 + arg8), arg9.select(1, 2, 3, 4, 5, 6))}
+// Column 35                      |
+object A {
+function(arg1, arg2(arg3(arg4,
+  arg5,
+  "arg6"), arg7 + arg8),
+arg10.select(1, 2, 3, 4, 5, 6))
+}
+
+
+
+
