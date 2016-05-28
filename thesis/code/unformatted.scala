132d3a67ba8922c4d91550f3f42a32726651834c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-05-28 14:33:35 +0200

    WIP

--- thesis/code/unformatted.scala
+++ thesis/code/unformatted.scala
@@ -1,9 +1,8 @@
 // Column 35                      |
-object A {
-function(arg1, arg2(arg3(arg4,
-  arg5,
-  "arg6"), arg7 + arg8),
-arg10.select(1, 2, 3, 4, 5, 6))
+object ScalafmtExample {
+function(arg1, arg2(arg3(
+  "String literal", arg4, arg5),
+arg6 + arg7))
 }
 
 
