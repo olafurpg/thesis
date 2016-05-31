75e826b6ac0aca9f172f3050a0e49fc518878e85
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-05-31 17:40:44 +0200

    WIP

--- thesis/code/immutable.scala
+++ thesis/code/immutable.scala
@@ -1,3 +1,3 @@
-List(1, 2, 3)
-  .map(_ + 1) // List(2, 3, 4)
-  .filter(_ > 2) // List(3, 4)
+val input = List(1, 2, 3)
+val output = input.map(_ + 1)    // List(2, 3, 4)
+                  .filter(_ > 2) // List(3, 4)
