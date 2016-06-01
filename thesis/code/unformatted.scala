ff919eec98b227d1798d892448ca81cf81326df0
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-01 20:04:46 +0200

    WIP

--- thesis/code/unformatted.scala
+++ thesis/code/unformatted.scala
@@ -1,10 +1,8 @@
 // Column 35                      |
 object ScalafmtExample {
   function(arg1, arg2(arg3(
-    "String literal", arg4, arg5),
-    arg6 + arg7))
+    "String literal"),
+    arg4 + arg5))
 }
-
-
 
 
