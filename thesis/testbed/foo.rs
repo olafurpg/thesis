0338dfd814305b5b122c5861edfe31a17e59e2e5
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-02 12:51:16 +0200

    WIP

--- thesis/testbed/foo.rs
+++ thesis/testbed/foo.rs
@@ -2,16 +2,11 @@
 
 impl Spanned for ast::Arg {
     fn span(&self) -> Span {
-        if items::is_named_arg(self) {
-          { { { { {
-          { { { { {
-	function(arg1, arg2(arg3(
-		"String literal", arg4, arg5),
-		arg6+arg7))
-          }}}}}
-          }}}}}
-        } else {
-            self.ty.span
-        }
+          aaaaaaaaaatoJson(
+                  balancingCharges.map(|balancingCharge|
+                          halResource(
+                                    obj(),
+                                            Seq(HalLink("self",
+                                                          selfEmploymentSummaryTypeIdHref(saUtr, taxYear, seId, BalancingChargesSummaryType, balancingCharge.id.get))))))
     }
 }
--- thesis/testbed/foo.scala
+++ thesis/testbed/foo.scala
@@ -1,12 +1,16 @@
 // column 80                                                                   |
 object Foo {
-Defn.Object(Nil,
-Term.Name("State"), Foo.Bar(1, Template(Nil, Seq( Ctor.Ref.Name("ScalaFmtLogger")),
-Term.Param(
-Nil,
-Name.Anonymous(), None, None)),
-Some()), Term.Name("gofmt") )
-err := format(fileSet, file, sourceAdj, indentAdj, src, printer.Config(Mode= printerMode, Tabwidth= tabWidth))
-
-
+  aaaaaaaaaatoJson(
+      balancingCharges.map(
+          balancingCharge =>
+            halResource(
+                obj(),
+                Seq(HalLink(
+                        "self",
+                        selfEmploymentSummaryTypeIdHref(
+                            saUtr,
+                            taxYear,
+                            seId,
+                            BalancingChargesSummaryType,
+                            balancingCharge.id.get))))))
 }
