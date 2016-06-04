0306a9faa4314186fe776d0112efe5ec7725e85f
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-05 01:06:51 +0200

    Best-first search

--- thesis/code/bfsv1.scala
+++ thesis/code/bfsv1.scala
@@ -1,14 +1,14 @@
 /** @returns Splits that produce and optimal formatting layout */
 def bestFirstSearch(formatTokens: FormatTokens, router: Router): List[Split] = {
   val lastFormatToken = formatTokens.last
-  val Q = PriorityQueue(State(formatTokens.head))
+  val Q = mutable.PriorityQueue(State.init(formatTokens.head))
   while (Q.nonEmpty) {
     val currentState = Q.pop
     if (currentState.formatToken == lastFormatToken) {
       return currentState.splits // reached the final state.
     } else {
       val decisions = router.getSplits(currentState.formatToken)
-      for (decision <- decisions) {
+      decisions.foreach { decision =>
         Q += State.nextState(currentState, decision)
       }
     }
---
+++ thesis/code/exponential.scala
@@ -0,0 +1,5 @@
+// Column 60                                               |
+a + b + c + d + e + f + g + h + i + j + k + l +
+m + n + o + p + q + r + s + t + v + w + y +
+// This comment exceeds column limit, no matter what path is chosen.
+z
