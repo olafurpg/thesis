9d1b502f14b0ccb2785f3c570a1265112dfe8c54
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-07 14:42:45 +0200

    Algorithms complete!!

--- thesis/code/align-alg.scala
+++ thesis/code/align-alg.scala
@@ -1,15 +1,16 @@
 case class FormatLocation(formatToken: FormatToken, split: Split, state: State)
-/** Returns true if this location is eligible for vertical alignment */
+/** Returns true if location is eligible for vertical alignment */
 def isCandidate(location: FormatLocation): Boolean
-/** Returns true if all alignment candidates match */
+/** Returns true if all vertical alignment candidates in a and b match */
 def allColumnsMatch(a: Array[FormatLocation], b: Array[FormatLocation]): Boolean
-/** Returns the index at which each column should align */
-def getMaxColumns(block: Vector[Array[formatLocation]]): Map[Int, Int]
+/** Returns map where the keys are (0 to block.length) and values are the
+    corresponding column index where all candidates should align */
+def getMaxColumns(block: Vector[Array[FormatLocation]]): Map[Int, Int]
 
 def getAlignTokens(
     locations: Array[FormatLocation],
-    alignConfiguration: Map[String, Regex]): Map[FormatToken, Int] = {
-  val finalResult = Map.newBuilder[FormatToken, Int]
+    alignConfiguration: Map[String, Regex]): Map[Split, Int] = {
+  val finalResult = Map.newBuilder[Split, Int]
   val lines: Array[Array[FormatLocation]] = getLines(locations)
   var block = Vector.empty[Array[FormatLocation]]
   for (formatLocations <- lines) {
@@ -22,8 +23,8 @@ def getAlignTokens(
       } else { // release alignment
         val maxColumns = getMaxColumns(block)
         for (line <- block) {
-          for ((tokenToAlign: FormatToken, columnIndex: Int) <- line.zipWithIndex) {
-            finalResult += (tokenToAlign,
+          for ((tokenToAlign, columnIndex) <- line.zipWithIndex) {
+            finalResult += (tokenToAlign.split,
                             maxColumns(columnIndex) - tokenToAlign.state.column)
           }
         }
