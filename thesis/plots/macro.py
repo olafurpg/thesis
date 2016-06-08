a3123582cc36990596aeb977f96a4c43f4068112
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-08 18:49:23 +0200

    Pretty tables.

--- thesis/plots/macro.py
+++ thesis/plots/macro.py
@@ -3,7 +3,10 @@ import csv, sys
 rows = list(csv.reader(sys.stdin))
 
 def get_row(row):
-  return "  " + " & ".join(row)
+    if row[0] == "hline":
+        return "\\hline\n" +  "  " + " & ".join(row[1:])
+    else:
+        return "  " + " & ".join(row)
 
 def hline():
   print "  \\hline"
@@ -12,6 +15,7 @@ cols = len(rows[0])
 
 print "\\begin{tabular}{" + ("l" * cols) + "}"
 print get_row(rows[0]) +  "\\\\"
+hline()
 hline()
 to_print = [get_row(row) for row in rows[1:]]
 print "\\\\\n".join(to_print)
