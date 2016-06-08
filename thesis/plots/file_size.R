8f35377699e5082a959aeff15c7147792c29a441
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-08 14:05:38 +0200

    Methods

--- thesis/plots/file_size.R
+++ thesis/plots/file_size.R
@@ -1,7 +1,17 @@
+# library(stargazer, lib.loc="plots")
 library(xtable, lib.loc="plots")
+# library(Hmisc, lib.loc="plots")
+
 options(xtable.floating = FALSE)
 options(xtable.timestamp = "")
 sizes <- read.table("data/file_sizes.dat")
-summary(sizes)
-xtable(quantile(sizes$V1, c(.50, .75, .90, .99)))
+# summary(sizes)
+sizes <- quantile(sizes$V1, c(.50, .75, .90, .99))
+# append(sizes, 1)
+# sizes
 
+# summary(sizes)
+# colnames(sizes) = c("Mean", "3rd Q", "90th", "99th")
+sizes
+# summary(sizes)
+# xtable(summary(sizes))
