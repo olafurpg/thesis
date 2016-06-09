f8ddb47362c18dcd2a90664923f21df190e306d0
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-06-09 18:35:37 +0200

    WIP

--- thesis/plots/summarize.py
+++ thesis/plots/summarize.py
@@ -3,7 +3,11 @@ import pandas as pd
 import sys
 
 pd.set_option("precision", 5)
-data = [int(line.split()[0]) for line in sys.stdin.readlines()]
+data = []
+for line in sys.stdin.readlines():
+    n = int(line.split()[0])
+    if n > 0:
+        data.append(n)
 df = pd.DataFrame({"sizes": data})
 summary = (df.describe(percentiles=[0.25, 0.50, 0.75, 0.90, 0.95, 0.99])
         .drop([ "count", "min", "std" ], 0)
