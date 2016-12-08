8eccb56485637e92edaf62bbdb3499fd4c6df144
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-12-08 17:40:55 +0000

    WIP

--- slides/foo.scala
+++ slides/foo.scala
@@ -3,4 +3,11 @@ object a {
   val user = User()
   val msg = s"$user logged out"
   List(1,2,3).tail
+  def liftedType = Unit
+  def doComputation() = Unit
+
+  def main(args: Seq[String]) {
+  }
+  lazy val x = doComputation()
+  implicit val tt = liftedType
 }
