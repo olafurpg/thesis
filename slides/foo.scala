4dcbbe771a4200c92695ae130b8a6aaf9f73f9e9
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-12-08 17:43:00 +0000

    WIP

--- slides/foo.scala
+++ slides/foo.scala
@@ -6,8 +6,12 @@ object a {
   def liftedType = Unit
   def doComputation() = Unit
 
-  def main(args: Seq[String]) {
+  def main(args: Seq[String]): Unit = {
   }
-  lazy val x = doComputation()
-  implicit val tt = liftedType
+
+  @volatile lazy val x = doComputation()
+
+  1
+
+  implicit val tt: TypedTree[Int] = liftedType
 }
